2470758a985184e17c6358368a7615dce197af40
Amanda Guerche Bruzadin <amanda.bruzadin@aluno.senai.br>
2026-04-17 08:32:21 -0300

    finalização carro.php
    teste para ver se ficou funcional e finalização

--- carro.php
+++ carro.php
@@ -1,28 +1,32 @@
 <?php
-class Carro {  
-    public $modelo;
+class Carro
+{
+    private $modelo;
     private $velocidade;
 
-    public function __construct($modelo, $velocidade) {
+    public function __construct($modelo, $velocidade)
+    {
         $this->modelo = $modelo;
         $this->velocidade = $velocidade;
     }
 
+    public function getModelo()
+    {
+        return $this->modelo;
+    }
+
     public function getVelocidade()
     {
-        return $this->velocidade; 
+        return $this->velocidade;
     }
 
     public function setVelocidade($novaVelocidade)
     {
-        if ($novaVelocidade >= 0 && $novaVelocidade <= 200)
-            {
-                $this-> velocidade = $novaVelocidade;
-            }
-            else 
-                {
-                    echo "Velocidade inválida, a nova velocidade deve ser até 200km/h!";
-                }
+        if ($novaVelocidade >= 0 && $novaVelocidade <= 200) {
+            $this->velocidade = $novaVelocidade;
+        } else {
+            echo "Velocidade inválida: $novaVelocidade. A nova velocidade deve ser até 200km/h! <br>";
+        }
     }
 }
 
@@ -30,9 +34,15 @@ class Carro {
 $meuCarro = new Carro("Senai-Mobile", 0);
 
 // O desastre: alteração direta sem validação
-{meuCarro->getVelocidade() = 5000}; // Velocidade de foguete?
-{$meuCarro->getVelocidade() = -60};   // Carro andando no tempo?
+{
+    $meuCarro->setVelocidade(100);
+} // Velocidade de foguete?
+{
+    $meuCarro->setVelocidade(5000);
+}   // Carro andando no tempo?
+{
+    $meuCarro->setVelocidade(-60);
+}   
 
-echo "Modelo: " . $meuCarro->modelo . "<br>";
+echo "Modelo: " . $meuCarro->getModelo(). "<br>";
 echo "Velocidade atual: " . $meuCarro->getVelocidade() . " km/h";
-?>
